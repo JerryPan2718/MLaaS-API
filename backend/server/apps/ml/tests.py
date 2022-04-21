c771a02816e9c4f48d10d80d12b579f0655ec1e8
Jerry Pan <jerrypan2718@berkeley.edu>
2022-04-21 15:44:26 -0700

    add ml code

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+import inspect
+from apps.ml.registry import MLRegistry
 
+from django.test import TestCase
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
 
 class MLTests(TestCase):
@@ -25,3 +27,23 @@ class MLTests(TestCase):
         self.assertEqual('OK', response['status'])
         self.assertTrue('label' in response)
         self.assertEqual('<=50K', response['label'])
+
+    def test_registry(self):
+        registry = MLRegistry()
+        self.assertEqual(len(registry.endpoints), 0)
+        endpoint_name = "income_classifier"
+        algorithm_object = RandomForestClassifier()
+        algorithm_name = "random forest"
+        algorithm_status = "production"
+        algorithm_version = "0.0.1"
+        algorithm_owner = "Piotr"
+        algorithm_description = "Random Forest with simple pre- and post-processing"
+        algorithm_code = inspect.getsource(RandomForestClassifier)
+        # add to registry
+        registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                    algorithm_status, algorithm_version, algorithm_owner,
+                    algorithm_description, algorithm_code)
+        # there should be one endpoint available
+        self.assertEqual(len(registry.endpoints), 1)
+
+        
